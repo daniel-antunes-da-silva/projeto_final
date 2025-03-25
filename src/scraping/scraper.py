c56ad7196fd4df31c644c99d3ef50ef6c4c4e580
Daniel Antunes <dev.daniel.antunes@gmail.com>
2025-03-24 22:44:01 -0300

    Etapa de scraping completa - 1ª versão

---
+++ src/scraping/scraper.py
@@ -0,0 +1,38 @@
+import requests
+import shutil
+import os
+
+
+def obter_arquivos(urls: list):
+    nomes_arquivos = []
+    for url in urls:
+        try:
+            resposta = requests.get(url)
+            # Separando a url através das barras, e pegando a última parte, referente ao nome do arquivo + extensão.
+            nome_arquivo = url.split('/')[-1]
+            nomes_arquivos.append(nome_arquivo)
+            with open(nome_arquivo, "wb") as file:
+                file.write(resposta.content)
+        except Exception as e:
+            print(f'Erro: {e}')
+    return nomes_arquivos
+
+
+def compactar_arquivos(arquivos: list):
+    nome_pasta = 'Arquivos PDF'
+    try:
+        os.mkdir(nome_pasta)
+    except FileExistsError:
+        pass
+    for arquivo in arquivos:
+        shutil.move(arquivo, nome_pasta)
+    shutil.make_archive('PDFs compactados', 'zip', nome_pasta)
+
+
+urls = ['https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de'
+        '-procedimentos/Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf',
+        'https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de'
+        '-procedimentos/Anexo_II_DUT_2021_RN_465.2021_RN628.2025_RN629.2025.pdf']
+
+arquivos = obter_arquivos(urls)
+compactar_arquivos(arquivos)
