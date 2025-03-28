70fca064f4faf39c5ef61bdfb95b1ad3745ceaef
Daniel Antunes <dev.daniel.antunes@gmail.com>
2025-03-27 22:29:48 -0300

    Add funcionalidade de extrair dados do pdf e converter para csv

---
+++ README.md
@@ -0,0 +1,2 @@
+# Requisitos:
+- java 8+ (mesmo o código sendo em Python, utiliza a biblioteca "tabula" que necessita do java).
---
+++ src/transformacao_de_dados/pdf_script.py
@@ -0,0 +1,44 @@
+import pdfplumber
+import pandas as pd
+
+
+def extrair_dados(caminho_pdf, pagina_inicial=3):
+    dados_tabela = []
+    with pdfplumber.open(caminho_pdf) as arquivo_pdf:
+        for indice, pagina in enumerate(arquivo_pdf.pages[pagina_inicial - 1:]):
+            print(f'Extraindo dados da página: {pagina_inicial + indice}')
+            tabela = pagina.extract_table()
+            if indice == 0:
+                # Pega os títulos da  PRIMEIRA tabela de Rol de procedimentos
+                titulos = tabela[0]
+                # Modificando os títulos de OD e AMB de acordo com a legenda, transformando
+                # em maiúsculo para manter a consistência.
+                titulos[3] = 'Seg. Odontológica'.upper()
+                titulos[4] = 'Seg. Ambulatorial'.upper()
+
+            # Extrai dados das páginas subsequentes e ignora os cabeçalhos repetidos.
+            if tabela:
+                for linha in tabela[1:]:
+                    # Modifica os dados (quando houver) de acordo com a legenda do arquivo.
+                    if linha[3] == 'OD':
+                        linha[3] = 'Seg. Odontológica'
+                    if linha[4] == 'AMB':
+                        linha[4] = 'Seg. Ambulatorial'
+                    dados_tabela.append(linha)
+                print(tabela)
+    return dados_tabela, titulos
+
+
+def converter_para_csv(dados, titulos, nome_saida):
+    df = pd.DataFrame(dados, columns=titulos)
+    df.to_csv(nome_saida, index=False, encoding='utf-8')
+    print(f'Arquivo "{nome_saida}" salvo no diretório atual.')
+
+
+if __name__ == '__main__':
+    pdf = r'..\web scraping\Arquivos PDF\Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf'
+    dados, titulos = extrair_dados(pdf, pagina_inicial=3)
+    converter_para_csv(dados, titulos, 'Anexo_I.csv')
+
+
+
